609cd19cb8842a49f30949f9bca580e1c2025d17
KingKayden <108900206+KingKayden1@users.noreply.github.com>
2025-01-29 00:12:05 -0600

    Update fxmanifest.lua
    1.5

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,9 @@ game 'gta5'
 
 author 'StarFallTeam'
 description 'A simple hide in trash script'
-version '0.1.0'
+version '0.1.5'
 
 client_script 'client.lua'
 shared_script 'config.lua'
+
+lua54 'yes'
